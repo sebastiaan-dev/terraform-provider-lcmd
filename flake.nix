f25a9fa033f41f04ee62d7c91090cd438271317e
Sebastiaan Gerritsen <dev@sebastiaan.io>
2025-12-02 21:40:49 +0100

    Initial working SDK

---
+++ flake.nix
@@ -0,0 +1,92 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    devenv-root = {
+      url = "file+file:///dev/null";
+      flake = false;
+    };
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    devenv.url = "github:cachix/devenv";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
+    vps-nix.url = "github:sebastiaan-dev/vps-nix";
+    vps-nix.flake = false;
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      devenv-root,
+      self,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          lib,
+          ...
+        }:
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+          _module.args.pkgs = import self.inputs.nixpkgs {
+            inherit system;
+            config.allowUnfreePredicate =
+              pkg:
+              builtins.elem (lib.getName pkg) [
+                "terraform"
+              ];
+          };
+
+          devenv.shells.default = {
+            name = "my-project";
+
+            languages.go.enable = true;
+
+            imports = [
+              # This is just like the imports in devenv.nix.
+              # See https://devenv.sh/guides/using-with-flake-parts/#import-a-devenv-module
+              # ./devenv-foo.nix
+              "${inputs.vps-nix}/devenv/python-module.nix"
+              "${inputs.vps-nix}/devenv/node-module.nix"
+            ];
+
+            # https://devenv.sh/reference/options/
+            packages = [ ];
+          };
+
+        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
--- internal/provider/example_resource.go
+++ internal/provider/example_resource.go
@@ -6,81 +6,104 @@ package provider
 import (
 	"context"
 	"fmt"
-	"net/http"
 
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
+
+	lzcsdk "gitee.com/linakesi/lzc-sdk/lang/go"
+	common "gitee.com/linakesi/lzc-sdk/lang/go/common"
+	"gitee.com/linakesi/lzc-sdk/lang/go/sys"
 )
 
 // Ensure provider defined types fully satisfy framework interfaces.
-var _ resource.Resource = &ExampleResource{}
-var _ resource.ResourceWithImportState = &ExampleResource{}
+var _ resource.Resource = &LpkResource{}
+var _ resource.ResourceWithImportState = &LpkResource{}
 
 func NewExampleResource() resource.Resource {
-	return &ExampleResource{}
+	return &LpkResource{}
 }
 
-// ExampleResource defines the resource implementation.
-type ExampleResource struct {
-	client *http.Client
+// LpkResource defines the resource implementation.
+type LpkResource struct {
+	client *LcmdClient
 }
 
-// ExampleResourceModel describes the resource data model.
-type ExampleResourceModel struct {
-	ConfigurableAttribute types.String `tfsdk:"configurable_attribute"`
-	Defaulted             types.String `tfsdk:"defaulted"`
-	Id                    types.String `tfsdk:"id"`
+// LpkResourceModel describes the resource data model.
+type LpkResourceModel struct {
+	Title     types.String `tfsdk:"title"`
+	LpkUrl    types.String `tfsdk:"lpk_url"`
+	LpkId     types.String `tfsdk:"lpk_id"`
+	Appid     types.String `tfsdk:"appid"`
+	Version   types.String `tfsdk:"version"`
+	Domain    types.String `tfsdk:"domain"`
+	Owner     types.String `tfsdk:"owner"`
+	Ephemeral types.Bool   `tfsdk:"ephemeral"`
 }
 
-func (r *ExampleResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
-	resp.TypeName = req.ProviderTypeName + "_example"
+func (r *LpkResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_lpk"
 }
 
-func (r *ExampleResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+func (r *LpkResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		// This description is used by the documentation generator and the language server.
-		MarkdownDescription: "Example resource",
+		MarkdownDescription: "LPK application resource for managing LPK packages on the system",
 
 		Attributes: map[string]schema.Attribute{
-			"configurable_attribute": schema.StringAttribute{
-				MarkdownDescription: "Example configurable attribute",
-				Optional:            true,
+			"lpk_url": schema.StringAttribute{
+				MarkdownDescription: "URL of the LPK package to install",
+				Required:            true,
 			},
-			"defaulted": schema.StringAttribute{
-				MarkdownDescription: "Example configurable attribute with default value",
+			"lpk_id": schema.StringAttribute{
+				MarkdownDescription: "ID of the LPK package",
+				Computed:            true,
+			},
+			"title": schema.StringAttribute{
+				MarkdownDescription: "Title of the LPK package",
+				Computed:            true,
+			},
+			"appid": schema.StringAttribute{
+				MarkdownDescription: "Application ID",
+				Computed:            true,
+			},
+			"version": schema.StringAttribute{
+				MarkdownDescription: "Version of the LPK package",
+				Computed:            true,
+			},
+			"domain": schema.StringAttribute{
+				MarkdownDescription: "Domain of the LPK package",
+				Computed:            true,
+			},
+			"owner": schema.StringAttribute{
+				MarkdownDescription: "Owner of the LPK package",
+				Computed:            true,
+			},
+			"ephemeral": schema.BoolAttribute{
+				MarkdownDescription: "Whether the LPK is ephemeral",
 				Optional:            true,
 				Computed:            true,
-				Default:             stringdefault.StaticString("example value when not configured"),
-			},
-			"id": schema.StringAttribute{
-				Computed:            true,
-				MarkdownDescription: "Example identifier",
-				PlanModifiers: []planmodifier.String{
-					stringplanmodifier.UseStateForUnknown(),
-				},
+				Default:             booldefault.StaticBool(false),
 			},
 		},
 	}
 }
 
-func (r *ExampleResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+func (r *LpkResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
 	// Prevent panic if the provider has not been configured.
 	if req.ProviderData == nil {
 		return
 	}
 
-	client, ok := req.ProviderData.(*http.Client)
+	client, ok := req.ProviderData.(*LcmdClient)
 
 	if !ok {
 		resp.Diagnostics.AddError(
 			"Unexpected Resource Configure Type",
-			fmt.Sprintf("Expected *http.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+			fmt.Sprintf("Expected *LcmdClient, got: %T. Please report this issue to the provider developers.", req.ProviderData),
 		)
 
 		return
@@ -89,99 +112,189 @@ func (r *ExampleResource) Configure(ctx context.Context, req resource.ConfigureR
 	r.client = client
 }
 
-func (r *ExampleResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
-	var data ExampleResourceModel
+func (r *LpkResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var data LpkResourceModel
 
-	// Read Terraform plan data into the model
 	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	// httpResp, err := r.client.Do(httpReq)
-	// if err != nil {
-	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to create example, got error: %s", err))
-	//     return
-	// }
+	sdkCtx := lzcsdk.WithRealUID(ctx, r.client.User)
+	boolVal := true
 
-	// For the purposes of this example code, hardcoding a response value to
-	// save into the Terraform state.
-	data.Id = types.StringValue("example-id")
+	ireq := &sys.InstallLPKRequest{
+		LpkUrl:       data.LpkUrl.ValueString(),
+		WaitUnitDone: &boolVal,
+	}
+	iresp, err := r.client.Gw.PkgManager.InstallLPK(sdkCtx, ireq)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to install LPK, got error: %s", err))
+		return
+	}
 
-	// Write logs using the tflog package
-	// Documentation: https://terraform.io/plugin/log
-	tflog.Trace(ctx, "created a resource")
+	tflog.Trace(ctx, "installed LPK package", map[string]any{
+		"lpk_url": ireq.LpkUrl,
+	})
 
-	// Save data into Terraform state
+	task := iresp.TaskInfo
+	apps, err := r.client.Gw.PkgManager.QueryApplication(sdkCtx, &sys.QueryApplicationRequest{
+		DeployIds: []string{*task.RealPkgId},
+	})
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to query application, got error: %s", err))
+		return
+	}
+
+	if len(apps.GetInfoList()) == 0 {
+		resp.Diagnostics.AddError("No applications found", "No applications found")
+		return
+	}
+	app := apps.GetInfoList()[0]
+
+	data.LpkId = types.StringValue(*task.RealPkgId)
+	data.Title = types.StringValue(app.GetTitle())
+	data.Version = types.StringValue(app.GetVersion())
+	data.Domain = types.StringValue(app.GetDomain())
+	data.Appid = types.StringValue(app.GetAppid())
+	data.Owner = types.StringValue(app.GetOwner())
+
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func (r *ExampleResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var data ExampleResourceModel
+func (r *LpkResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state LpkResourceModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	uidsResp, err := r.client.Gw.Users.ListUIDs(ctx, &common.ListUIDsRequest{})
+	if err != nil || len(uidsResp.GetUids()) == 0 {
+		resp.Diagnostics.AddError("UID lookup failed", "ListUIDs returned no entries or error")
+		return
+	}
+	sdkCtx := lzcsdk.WithRealUID(ctx, uidsResp.GetUids()[0])
+
+	appid := state.Appid.ValueString()
+	if appid == "" {
+		resp.State.RemoveResource(ctx)
+		return
+	}
+
+	apps, err := r.client.Gw.PkgManager.QueryApplication(sdkCtx, &sys.QueryApplicationRequest{
+		DeployIds: []string{appid},
+	})
+	if err != nil {
+		resp.Diagnostics.AddError("QueryApplication failed", err.Error())
+		return
+	}
+	if len(apps.GetInfoList()) == 0 {
+		resp.State.RemoveResource(ctx)
+		return
+	}
+
+	app := apps.GetInfoList()[0]
+	state.LpkId = types.StringValue(state.LpkId.ValueString())
+	state.Title = types.StringValue(app.GetTitle())
+	state.Version = types.StringValue(app.GetVersion())
+	state.Domain = types.StringValue(app.GetDomain())
+	state.Appid = types.StringValue(app.GetAppid())
+	state.Owner = types.StringValue(app.GetOwner())
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &state)...)
+}
+
+func (r *LpkResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan LpkResourceModel
+	var state LpkResourceModel
+
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	sdkCtx := lzcsdk.WithRealUID(ctx, r.client.User)
+
+	if plan.LpkUrl.ValueString() != state.LpkUrl.ValueString() {
+		if state.Appid.ValueString() != "" {
+			_, err := r.client.Gw.PkgManager.Uninstall(sdkCtx, &sys.UninstallRequest{
+				Appid:     state.Appid.ValueString(),
+				ClearData: false,
+			})
+			if err != nil {
+				resp.Diagnostics.AddError("Uninstall failed", err.Error())
+				return
+			}
+		}
+
+		wait := true
+		installResp, err := r.client.Gw.PkgManager.InstallLPK(sdkCtx, &sys.InstallLPKRequest{
+			LpkUrl:       plan.LpkUrl.ValueString(),
+			WaitUnitDone: &wait,
+		})
+		if err != nil {
+			resp.Diagnostics.AddError("Install failed", err.Error())
+			return
+		}
+
+		task := installResp.GetTaskInfo()
+		apps, err := r.client.Gw.PkgManager.QueryApplication(sdkCtx, &sys.QueryApplicationRequest{
+			DeployIds: []string{task.GetRealPkgId()},
+		})
+		if err != nil || len(apps.GetInfoList()) == 0 {
+			resp.Diagnostics.AddError("QueryApplication failed", "no app returned after reinstall")
+			return
+		}
+
+		app := apps.GetInfoList()[0]
+		plan.LpkId = types.StringValue(task.GetRealPkgId())
+		plan.Title = types.StringValue(app.GetTitle())
+		plan.Version = types.StringValue(app.GetVersion())
+		plan.Domain = types.StringValue(app.GetDomain())
+		plan.Appid = types.StringValue(app.GetAppid())
+		plan.Owner = types.StringValue(app.GetOwner())
+	} else {
+		plan.LpkId = state.LpkId
+		plan.Title = state.Title
+		plan.Version = state.Version
+		plan.Domain = state.Domain
+		plan.Appid = state.Appid
+		plan.Owner = state.Owner
+	}
+
+	plan.Ephemeral = types.BoolValue(plan.Ephemeral.ValueBool())
+	resp.Diagnostics.Append(resp.State.Set(ctx, &plan)...)
+}
+
+func (r *LpkResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var data LpkResourceModel
 
 	// Read Terraform prior state data into the model
 	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	// httpResp, err := r.client.Do(httpReq)
-	// if err != nil {
-	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read example, got error: %s", err))
-	//     return
-	// }
+	sdkCtx := lzcsdk.WithRealUID(ctx, r.client.User)
 
-	// Save updated data into Terraform state
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
-}
+	request := &sys.UninstallRequest{
+		Appid:     data.Appid.ValueString(),
+		ClearData: data.Ephemeral.ValueBool(),
+	}
 
-func (r *ExampleResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
-	var data ExampleResourceModel
-
-	// Read Terraform plan data into the model
-	resp.Diagnostics.Append(req.Plan.Get(ctx, &data)...)
-
-	if resp.Diagnostics.HasError() {
+	_, err := r.client.Gw.PkgManager.Uninstall(sdkCtx, request)
+	if err != nil {
+		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to uninstall LPK, got error: %s", err))
 		return
 	}
 
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	// httpResp, err := r.client.Do(httpReq)
-	// if err != nil {
-	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to update example, got error: %s", err))
-	//     return
-	// }
-
-	// Save updated data into Terraform state
-	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+	tflog.Trace(ctx, "deleted LPK package", map[string]any{
+		"lpk_url": data.LpkUrl.ValueString(),
+	})
 }
 
-func (r *ExampleResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var data ExampleResourceModel
-
-	// Read Terraform prior state data into the model
-	resp.Diagnostics.Append(req.State.Get(ctx, &data)...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	// If applicable, this is a great opportunity to initialize any necessary
-	// provider client data and make a call using it.
-	// httpResp, err := r.client.Do(httpReq)
-	// if err != nil {
-	//     resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to delete example, got error: %s", err))
-	//     return
-	// }
-}
-
-func (r *ExampleResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+func (r *LpkResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
 	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
 }
